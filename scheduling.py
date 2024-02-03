92a59d94d7ead4fc72d322b67d1357583bc34dca
panos03 <panos.haritsis@gmail.com>
2024-02-03 19:13:27 +0000

    Update scheduling.py

--- scheduling.py
+++ scheduling.py
@@ -1,37 +1,64 @@
 import numpy as np
 
+# TODO: put elsewhere
+class Soldier:
+    def __init__(self, soldier_id: str, sleep_scores: [float]):
+        self.soldier_id = soldier_id
+        self.__sleep_scores = sleep_scores
+
+        self.optimum_time = self.__get_optimum_time()
+
+    def __get_optimum_time(self):
+        # so far just based on sleep scores
+        return np.argmin(np.array(self.__sleep_scores))[0]
+
 class Schedule:
-    def __init__(self, agents: [int], total_interval: float, shift_length: float, sleep_times: [[float]]) -> None: 
-        # interval and shift length is in hours
-        # agents is the ids of the soldiers
-        self.agents = agents
+    def __init__(self, soldiers: [Soldier], slot_length: float, total_interval: float) -> None: 
+        # interval and slot length is in hours
+        if self.total_interval % slot_length != 0:
+            raise ValueError("Condition not satisfied during initialization, total_interval should be a multiple of shift_length")
+        self.soldiers = soldiers
+        self.slot_length = slot_length
         self.total_interval = total_interval
-        self.shift_length = shift_length
-        self.sleep_times = sleep_times
-
-        self.optimum_times = []
-        self.__get_optimum_times
-
-    def __get_optimum_times(self):
-        # so far just sleep
-        optimum_times = []
-        for sleep in range(len(self.sleep_times)):
-            optimum_times.append(max(sleep))
-        self.optimum_times = optimum_times
 
     def make_schedule(self):
-        # sort agents by optimum time
-        
+        # sort soldiers by optimum time
+        self.soldiers.sort(key=lambda x: x.optimum_time)
         # split total_interval into slots (by shift_length)
+        slots = []
+        for i in range(int(self.total_interval/self.slot_length)):
+            slots.append(i + (1/2) * self.slot_length)
+        # while soldiers, loop over slots (use central time of slot), add agent w closest optimum_time to slot
+        schedule = [[] * len(slots)]
+        break_loop = False
+        while not break_loop:
+            for s in slots:
+                if not self.soldiers:
+                    break_loop = True
+                    break
+                
+                
 
-        # while agents, loop over slots (use central time of slot), add agent w closest optimum_time to slot
-
-        # return slots (each slot has list of agents)
-        pass
+        # return slots (each slot has list of soldiers)
 
 
     # db_caller for data
     # intensity of sleep
-
+    def check_match(self, soft_limit, idx):
+        return idx in soft_limit[:10]
+        
+    def get_optimum_time():
+        
+    
+    def make_schedule0(self):
+        optimum_time = self.get_optimum_time()
+        sorted_optimum = np.argsort(optimum_time)
+        schedule = []
+        for i in range(int(self.total_interval/self.slot_length)):
+            time = (slot_length * i) % 24 
+            if check_match
+            schedule.append(self.agent)
     # soft constraints: sleep schedule peak times, same no of shifts across the board, i.e. uniformly distributed
     # things to think about: combination for metrics, make sure as much time is covered as possible
+    
+sch = Schedule([Soldier("1", [1,3,4])])
