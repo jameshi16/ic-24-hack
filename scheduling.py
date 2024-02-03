c2808a9ca7229a8955bc2428b47f10ff494a6a39
nicklimjx <114497929+nicklimjx@users.noreply.github.com>
2024-02-03 18:02:55 +0000

    Add files via upload

--- scheduling.py
+++ scheduling.py
@@ -1,13 +1,25 @@
+import numpy as np
+
 class Schedule:
-    def __init__(self, agents: [int], total_interval: float, shift_length: float) -> None: 
+    def __init__(self, agents: [int], total_interval: float, shift_length: float, sleep_times: [[float]]) -> None: 
         # interval and shift length is in hours
         # agents is the ids of the soldiers
         self.agents = agents
         self.total_interval = total_interval
         self.shift_length = shift_length
-        pass
+        self.sleep_time = sleep_times
 
-    # db_caller for data
+        pass
+    
+
+    def get_optimum(self):
+        optimum = []
+        for sleep in range(len(self.sleep_times)):
+            optimum.append(max(sleep))
+        return optimum
+
+
+
     # intensity of sleep
 
     # soft constraints: sleep schedule peak times, same no of shifts across the board, i.e. uniformly distributed
