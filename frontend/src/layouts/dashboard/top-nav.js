3eab41f1c6fb7b125b4ef929a5776fa346299452
James <lim.en.hui.james@gmail.com>
2024-02-03 21:10:28 +0000

    Remove search icon

--- frontend/src/layouts/dashboard/top-nav.js
+++ frontend/src/layouts/dashboard/top-nav.js
@@ -65,13 +65,6 @@ export const TopNav = (props) => {
                 </SvgIcon>
               </IconButton>
             )}
-            <Tooltip title="Search">
-              <IconButton>
-                <SvgIcon fontSize="small">
-                  <MagnifyingGlassIcon />
-                </SvgIcon>
-              </IconButton>
-            </Tooltip>
           </Stack>
           <Stack
             alignItems="center"
