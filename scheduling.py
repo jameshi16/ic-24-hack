bc1600800ec3b30ae308dd36774a7d8e532194c8
panos03 <panos.haritsis@gmail.com>
2024-02-03 18:13:19 +0000

    Update scheduling.py

--- scheduling.py
+++ scheduling.py
@@ -7,19 +7,30 @@ class Schedule:
         self.agents = agents
         self.total_interval = total_interval
         self.shift_length = shift_length
-        self.sleep_time = sleep_times
+        self.sleep_times = sleep_times
 
-        pass
-    
+        self.optimum_times = []
+        self.__get_optimum_times
 
-    def get_optimum(self):
-        optimum = []
+    def __get_optimum_times(self):
+        # so far just sleep
+        optimum_times = []
         for sleep in range(len(self.sleep_times)):
-            optimum.append(max(sleep))
-        return optimum
+            optimum_times.append(max(sleep))
+        self.optimum_times = optimum_times
+
+    def make_schedule(self):
+        # sort agents by optimum time
+        
+        # split total_interval into slots (by shift_length)
+
+        # while agents, loop over slots (use central time of slot), add agent w closest optimum_time to slot
+
+        # return slots (each slot has list of agents)
+        pass
 
 
-
+    # db_caller for data
     # intensity of sleep
 
     # soft constraints: sleep schedule peak times, same no of shifts across the board, i.e. uniformly distributed
